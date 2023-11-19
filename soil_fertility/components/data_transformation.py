5b0723ec027da681855711f354cdb29b7c4580d2
RedhaWassim <rwasssim@gmail.com>
2023-11-19 23:30:11 +0100

    update transformation

--- soil_fertility/components/data_transformation.py
+++ soil_fertility/components/data_transformation.py
@@ -2,7 +2,14 @@ from pydantic import BaseModel
 from soil_fertility.components.path_config import PathConfig
 from typing import Literal, List
 from sklearn.pipeline import Pipeline
-
+from soil_fertility.components.transformations import (
+    DropMissingValues,
+    DropDuplicates,
+    CustomImputer,
+    MinMaxTransformation,
+    ZScoreTransformation
+)
+from sklearn.compose import ColumnTransformer
 
 class DataTransformationConfig(PathConfig):
     def __init__(self):
@@ -14,32 +21,94 @@ class DataTransformationConfig(PathConfig):
 class DataTransformation(BaseModel):
     transformation_config: DataTransformationConfig = DataTransformationConfig()
 
-    def generate_transformer(
-        self, numerical_features: List[str], categorical_features: List[str]
+    def generate_transformer_first_data(
+        self, numerical_features: List[str], all_features: List[str]
     ) -> None:
         try:
+            all_features_pipeline = Pipeline(
+                [
+                    ("drop_missing_values", DropMissingValues()),
+                    ("drop_duplicates", DropDuplicates()),
+                    ("custom_imputer", CustomImputer()),
+                ]
+            )
+
             numerical_pipeline = Pipeline(
                 [
-                    ("imputer", SimpleImputer(strategy="median")),
-                    ("scaler", StandardScaler()),
+                    ("imputer", MinMaxTransformation(strategy="median")),
                 ]
             )
-            categorical_pipeline = Pipeline(
-                [
-                    ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("encoder", OneHotEncoder(handle_unknown="ignore")),
-                ]
-            )
+
             preprocessor = ColumnTransformer(
                 [
+                    ("all_features", all_features_pipeline, all_features),
                     ("numerical", numerical_pipeline, numerical_features),
-                    ("categorical", categorical_pipeline, categorical_features),
                 ]
             )
             return preprocessor
 
         except:
             pass
+        
+    def generate_transformer_second_data(
+        self, numerical_features: List[str], all_features: List[str]
+    ) -> None:
+        try:
+            all_features_pipeline = Pipeline(
+                [
+                    ("drop_missing_values", DropMissingValues()),
+                    ("drop_duplicates", DropDuplicates()),
+                    ("custom_imputer", CustomImputer()),
+                ]
+            )
+
+            numerical_pipeline = Pipeline(
+                [
+                    ("imputer", MinMaxTransformation(strategy="median")),
+                ]
+            )
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("all_features", all_features_pipeline, all_features),
+                    ("numerical", numerical_pipeline, numerical_features),
+                ]
+            )
+            return preprocessor
+
+        except:
+            pass
+
+
+    def generate_transformer_third_data(
+        self, numerical_features: List[str], all_features: List[str]
+    ) -> None:
+        try:
+            all_features_pipeline = Pipeline(
+                [
+                    ("drop_missing_values", DropMissingValues()),
+                    ("drop_duplicates", DropDuplicates()),
+                    ("custom_imputer", CustomImputer()),
+                ]
+            )
+
+            numerical_pipeline = Pipeline(
+                [
+                    ("imputer", MinMaxTransformation(strategy="median")),
+                ]
+            )
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("all_features", all_features_pipeline, all_features),
+                    ("numerical", numerical_pipeline, numerical_features),
+                ]
+            )
+            return preprocessor
+
+        except:
+            pass
+
 
     def transform(self) -> None:
         pass
